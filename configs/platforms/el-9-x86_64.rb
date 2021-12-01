9fc268bdeb630ab0b31aacc8f6c5250fb92021cf
Steve Traylen <steve.traylen@cern.ch>
2021-12-01 16:06:29 +0100

    CERN and CentOS 9 platform

---
+++ configs/platforms/el-9-x86_64.rb
@@ -0,0 +1,10 @@
+platform "el-9-x86_64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  packages = ['make', 'rsync', 'rpm-build']
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "centos-9-x86_64"
+end
